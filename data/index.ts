b2e56a541489b7b67aaae5a6f6be91ce974b07ee
Рафаэль Насыбуллин <crazynihonjin97@gmail.com>
2024-10-10 18:34:14 +0500

    added:structure of files

---
+++ data/index.ts
@@ -0,0 +1,16 @@
+export const data = [
+  {
+    "id": 1,
+    "name": "Smartphone",
+    "category": "Electronics",
+    "price": 300,
+    "image": "url-to-image"
+  },
+  {
+    "id": 2,
+    "name": "Book",
+    "category": "Books",
+    "price": 15,
+    "image": "url-to-image"
+  },
+]
